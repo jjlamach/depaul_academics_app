a842e83c43ecab5dba5b1f0ff111508b71f3d300
julama <lamajulio17@gmail.com>
2023-09-06 18:23:03 -0500

    Logo changed

--- lib/app/screens/home/widgets/home_page_form_page.dart
+++ lib/app/screens/home/widgets/home_page_form_page.dart
@@ -1,7 +1,4 @@
-import 'package:depaul_campus_connect/app/screens/home/widgets/text_form_field_ccid.dart';
-import 'package:depaul_campus_connect/app/screens/home/widgets/text_form_field_password.dart';
 import 'package:depaul_campus_connect/common/dimens.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -16,14 +13,17 @@ class _HomePageFormState extends State<HomePageForm> {
   @override
   Widget build(BuildContext context) {
     return SignInScreen(
+      providers: [
+        EmailAuthProvider(),
+      ],
       headerMaxExtent: MediaQuery.of(context).size.height * 0.3,
       headerBuilder: (context, constraints, shrinkOffset) {
         return Padding(
-          padding: const EdgeInsets.all(30.0),
+          padding: const EdgeInsets.only(top: 70.0),
           child: ClipRRect(
             borderRadius: BorderRadius.circular(Dimens.borderRadius),
             child: Image.asset(
-              'lib/assets/depaul_logo.jpg',
+              'lib/assets/DULOGO_no_background.png',
             ),
           ),
         );
