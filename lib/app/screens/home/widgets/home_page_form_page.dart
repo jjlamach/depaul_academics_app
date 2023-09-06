9ddb505e5789995978608a8ce085807f8e8797f1
julama <lamajulio17@gmail.com>
2023-09-06 18:09:30 -0500

    Firebase UI authentication
    Used Firebase UI for authentication

--- lib/app/screens/home/widgets/home_page_form_page.dart
+++ lib/app/screens/home/widgets/home_page_form_page.dart
@@ -1,6 +1,8 @@
 import 'package:depaul_campus_connect/app/screens/home/widgets/text_form_field_ccid.dart';
 import 'package:depaul_campus_connect/app/screens/home/widgets/text_form_field_password.dart';
 import 'package:depaul_campus_connect/common/dimens.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePageForm extends StatefulWidget {
@@ -13,70 +15,19 @@ class HomePageForm extends StatefulWidget {
 class _HomePageFormState extends State<HomePageForm> {
   @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Builder(
-        builder: (formContext) => Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(Dimens.borderRadius),
-              child: Image.asset(
-                'lib/assets/depaul_logo.jpg',
-                height: 200,
-                width: 200,
-              ),
+    return SignInScreen(
+      headerMaxExtent: MediaQuery.of(context).size.height * 0.3,
+      headerBuilder: (context, constraints, shrinkOffset) {
+        return Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(Dimens.borderRadius),
+            child: Image.asset(
+              'lib/assets/depaul_logo.jpg',
             ),
-            const SizedBox(height: 50),
-            const TextFormFieldCampusConnectID(
-              validatorMessage: "Missing ID",
-              hintTextMessage: "Campus Connect ID",
-            ),
-            const SizedBox(height: 20),
-            const TextFormFieldPassword(
-              validatorMessage: "Missing password",
-              hintTextMessage: "Password",
-            ),
-            const SizedBox(height: 50),
-            OutlinedButton(
-              style: ButtonStyle(
-                shape: MaterialStatePropertyAll(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(Dimens.borderRadius),
-                  ),
-                ),
-                backgroundColor: MaterialStatePropertyAll(
-                  Theme.of(context).colorScheme.primary,
-                ),
-                padding: const MaterialStatePropertyAll(
-                  EdgeInsets.symmetric(
-                    horizontal: 100,
-                    vertical: 15,
-                  ),
-                ),
-                foregroundColor: MaterialStatePropertyAll(
-                  Theme.of(context).colorScheme.secondary,
-                ),
-              ),
-              onPressed: () => checkFormFields(formContext),
-              child: const Text(
-                "Log In",
-                style: TextStyle(
-                  fontSize: Dimens.fontSizeButtons,
-                  color: Colors.white,
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
-  }
-
-  void checkFormFields(BuildContext context) {
-    final loginForm = Form.of(context);
-    final isFormValid = loginForm.validate();
-    if (isFormValid) {
-      loginForm.save();
-    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,34 @@
 import 'package:depaul_campus_connect/app/screens/home/home_page.dart';
+import 'package:depaul_campus_connect/common/dimens.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart' as FirebaseUIAuth;
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromRGBO(14, 65, 115, 1),
 );
 
-void main() {
-  runApp(const DePaulUCampusConnect());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setUpFirebase();
+  setUpAuthProviders();
+  runApp(
+    const DePaulUCampusConnect(),
+  );
+}
+
+Future<FirebaseApp> setUpFirebase() async {
+  return await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+    name: "depaul-campus-connect",
+  );
+}
+
+void setUpAuthProviders() {
+  FirebaseUIAuth.FirebaseUIAuth.configureProviders([
+    FirebaseUIAuth.EmailAuthProvider(),
+  ]);
 }
 
 class DePaulUCampusConnect extends StatelessWidget {
@@ -18,6 +40,32 @@ class DePaulUCampusConnect extends StatelessWidget {
     return MaterialApp(
       title: 'Mini DU Campus Connect',
       theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(Dimens.borderRadius),
+          ),
+        ),
+        outlinedButtonTheme: const OutlinedButtonThemeData(
+          style: ButtonStyle(
+            padding: MaterialStatePropertyAll(EdgeInsets.all(15.0)),
+            backgroundColor: MaterialStatePropertyAll(
+              Color.fromRGBO(14, 65, 115, 1),
+            ),
+            foregroundColor: MaterialStatePropertyAll(Colors.white),
+            textStyle: MaterialStatePropertyAll(
+              TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            shape: MaterialStatePropertyAll(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(Dimens.borderRadius),
+                ),
+              ),
+            ),
+          ),
+        ),
         colorScheme: kColorScheme,
         useMaterial3: true,
       ),
