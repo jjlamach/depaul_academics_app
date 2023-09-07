9b279080c0ca350450000a843c0b7af7b7a07c62
julama <lamajulio17@gmail.com>
2023-09-06 20:27:08 -0500

    Sign out logic

--- lib/app/screens/home/home_page.dart
+++ lib/app/screens/home/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:depaul_campus_connect/app/screens/signIn/sign_in_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,8 +10,17 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: const Center(
-        child: Text("Home Page screen"),
+      body: Center(
+        child: TextButton(
+          onPressed: () async {
+            FirebaseAuth.instance.signOut().then(
+              (value) {
+                Navigator.of(context).pushReplacementNamed("/");
+              },
+            );
+          },
+          child: Text("SignedOut"),
+        ),
       ),
     );
   }
--- lib/app/screens/signIn/sign_in_form_page.dart
+++ lib/app/screens/signIn/sign_in_form_page.dart
@@ -1,3 +1,4 @@
+import 'package:depaul_campus_connect/app/screens/home/home_page.dart';
 import 'package:depaul_campus_connect/common/dimens.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
