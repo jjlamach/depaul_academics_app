7a4847dba98e7ab40ad197499f0ddce9ceb3c07b
julama <lamajulio17@gmail.com>
2023-09-08 12:51:32 -0500

    Changed mockend.yml

--- lib/app/screens/courses/bloc/courses_cubit.dart
+++ lib/app/screens/courses/bloc/courses_cubit.dart
@@ -7,11 +7,12 @@ part 'courses_cubit.freezed.dart';
 
 class CoursesCubit extends Cubit<CoursesState> {
   final GetCoursesUseCase _useCase;
-  CoursesCubit(this._useCase) : super(_Initial());
+  CoursesCubit(this._useCase) : super(_Loading());
 
   void getCourses() async {
     try {
       final response = await _useCase.getCourses();
+      emit(CoursesState.loading());
       emit(CoursesState.loaded(response));
     } on Exception catch (error) {
       emit(CoursesState.error(error));
--- lib/app/screens/courses/courses_page.dart
+++ lib/app/screens/courses/courses_page.dart
@@ -1,5 +1,4 @@
 import 'package:depaul_campus_connect/app/screens/courses/bloc/courses_cubit.dart';
-import 'package:depaul_campus_connect/domain/models/courses.dart';
 import 'package:depaul_campus_connect/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -15,19 +14,30 @@ class CoursesPage extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.primary,
         ),
-        body: ListView.separated(
-          separatorBuilder: (context, index) => const SizedBox(height: 10.0),
-          itemCount: 4,
-          padding: const EdgeInsets.all(10.0),
-          itemBuilder: (context, index) => Container(
-            height: 50.0,
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.secondary,
-            ),
-            child: BlocBuilder<CoursesCubit, CoursesState>(
-              builder: (context, state) => Text('State: $state'),
-            ),
-          ),
+        body: BlocBuilder<CoursesCubit, CoursesState>(
+          builder: (context, state) {
+            return state.maybeWhen(
+              loading: () => Center(
+                child: CircularProgressIndicator(
+                  color: Colors.red,
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+              loaded: (courses) => ListView.separated(
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 10.0),
+                itemCount: 4,
+                padding: const EdgeInsets.all(10.0),
+                itemBuilder: (context, index) => Container(
+                  height: 50.0,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                ),
+              ),
+              orElse: () => const SizedBox.shrink(),
+            );
+          },
         ),
       ),
     );
